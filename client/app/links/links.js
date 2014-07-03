4b67476ba6aa12ea4c7bdf25a7423d53086778a6
Hack Reactor Students <grimi94@gmail.com>
2014-07-02 17:10:23 -0700

    Completed tests for LinksController, Links service, and routing for Links and Shorten template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,11 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, Links, $http) {
+  $scope.data = { links : [] };
+  $scope.getLinks = function(){
+    Links.getLinks().then(function(data){
+      $scope.data.links = data;
+    });
+  };
+  $scope.getLinks();
 });
